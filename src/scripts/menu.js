7bf44a226a6d5decd07a769fc9eee64e3330f045
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2024-05-07 22:51:16 -0400

    more changes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,6 @@
 document.addEventListener("astro:page-load", () => {
   document.querySelector(".hamburger").addEventListener("click", () => {
-    document.querySelector(".nav-links").classList.toggle("expanded");
+    const nav = document.querySelector(".nav-links");
+    nav.classList.toggle("expanded");
   });
 });

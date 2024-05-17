e9e89fdb36805076552512487e481eb91d97f784
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2024-05-16 20:39:03 -0400

    add full content to rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("posts");
@@ -8,12 +11,27 @@ export async function GET(context) {
     title: "Francesco Menghi | Blog",
     description: "My personal blog",
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/blog/${post.slug}`,
-    })),
+    trailingSlash: false,
+    items: posts.map((post) => {
+      let contentHtml = sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      });
+
+      if (post.data.image) {
+        const coverImageHtml = `<img src="${post.data.image.url}" alt="${post.data.image.alt}" />`;
+        contentHtml = `${coverImageHtml}\n${contentHtml}`;
+      }
+
+      return {
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `/blog/${post.slug}`,
+        content: contentHtml,
+        ...post.data,
+      };
+    }),
     customData: `<language>en-us</language>`,
+    stylesheet: "/pretty-feed-v3.xsl",
   });
 }

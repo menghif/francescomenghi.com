8f38937ed482fb8723c8d5358a5ebae4e3db4a74
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2024-06-02 21:21:49 -0400

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://francescomenghi.com",
-  integrations: [expressiveCode()],
+  integrations: [expressiveCode(), sitemap()]
 });

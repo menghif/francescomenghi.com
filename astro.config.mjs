a6087d710aba8e07a2547bb4dc4f451716d3e317
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2024-07-04 18:28:33 -0400

    force expressiveCode to always use github-dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://francescomenghi.com",
-  integrations: [expressiveCode(), sitemap()]
+  integrations: [
+    expressiveCode({
+      themes: ["github-dark"],
+    }),
+    sitemap(),
+  ],
 });

eef57a0ae3ab9334dcbdaa3019453edbebd02109
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2024-10-21 17:19:17 -0400

    use fontaine for better font fallback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig, passthroughImageService } from "astro/config";
+import { FontaineTransform } from "fontaine";
 
 import expressiveCode from "astro-expressive-code";
 import sitemap from "@astrojs/sitemap";
@@ -12,6 +13,15 @@ export default defineConfig({
     }),
     sitemap(),
   ],
+  vite: {
+    plugins: [
+      FontaineTransform.vite({
+        fallbacks: ["Arial"],
+        // eslint-disable-next-line no-undef
+        resolvePath: (id) => new URL(`./public${id}`, import.meta.url), // id is the font src value in the CSS
+      }),
+    ],
+  },
   image: {
     service: passthroughImageService(),
   },
